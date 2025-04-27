8fc9c04d905a66e86a98cef43ac77daa10d4c664
beeravalli-417 <beeravalli345@gmail.com>
2025-04-27 12:03:31 +0530

    second change

--- day_1_datatypes.py
+++ day_1_datatypes.py
@@ -1,7 +1,43 @@
-str
+"""str
 int
 bool
 float
 dict
 Decimal
-Date
+Date"""
+from operator import truediv
+
+"""age = 23
+year = 2025
+quantity = -10
+print(age)"""
+
+"""name = "brahma"
+message = "Hello, World!"
+city = 'hyd'
+print(message)"""
+"""name = "brahma"
+message = "Hello, World!"
+city = 'hyd'
+print(name)"""
+"""is_active = True
+is_logged_in = False
+has_permission = True
+print(is_active)"""
+"""price = 19.99
+pi = 3.14159
+temperature = -5.4
+print(temperature)"""
+"""user = {
+    "name": "brahma",
+    "age": 24,
+    "is_admin": False
+}
+
+product = {
+    "id": 101,
+    "name": "Laptop",
+    "price": 799.99
+}
+print("name")"""
+
