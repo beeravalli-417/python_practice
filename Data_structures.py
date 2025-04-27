bdc2b2af553b20563c51e2c420c54f2fc4ae75c9
beeravalli-417 <beeravalli345@gmail.com>
2025-04-27 11:59:11 +0530

    second change

--- Data_structures.py
+++ Data_structures.py
@@ -3,3 +3,57 @@ tuple
 set
 dict
 
+#identityoperators
+"""x = [1, 2, 3]
+y = [4, 5, 6]
+z = y
+
+print(x is x)    # false (different object)
+print(y is y)    # False (different objects even if same data)
+print(x is y)    # false (data is equal)"""
+#membership operator
+"""colors = ["red", "blue", "green"]
+
+print("red" in colors)     # True
+print("yellow"  in colors)  # True"""
+#bitwise operators
+"""a = 10  # (binary: 0101)
+b = 3  # (binary: 0011)
+print(a & b)  
+print(a | b)  
+print(a ^ b)  
+print(~a)    
+print(a << 1) 
+print(a >> 1) """
+#arthematic operator
+"""a = 20
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)"""
+#comparasion operator
+"""x = 5
+y = 10
+print(x == y)  
+print(x != y)  
+print(x > y)   
+print(x < y)   
+print(x >= y)  
+print(x <= y) """
+#logical operator
+"""age = 20
+print(age > 18 and age < 30)  
+print(age > 25 or age < 18)   
+print(not (age > 18))  """
+
+
+
+
+
+
+
+
