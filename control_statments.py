389cb5ed8740749a27f20b831e6fc44ec7255f88
beeravalli-417 <beeravalli345@gmail.com>
2025-04-27 11:48:45 +0530

    second modified

--- control_statments.py
+++ control_statments.py
@@ -1,3 +1,80 @@
-if
+"""if
 elif
-else
+else"""
+"""follwers = 6
+follwers = follwers**2
+print(follwers)"""
+"""follwers = 9
+follwers = follwers**3
+follwers**3
+print(follwers)"""
+"""age = 16
+
+if age >= 16:
+    print("You can vote!")
+else:
+    print("You are too young to vote.")"""
+"""for i in range(5):
+        print(i)"""
+"""age = int(input("Enter your age: "))
+
+#if age >= 18:
+#    print("You can use your rights.")
+#else:
+#    print("You cannot use your rights yet.")"""
+#age = int(input("Enter your age: "))
+
+"""if age >= 18:
+    print("You can use your rights.")
+else:
+    print("You cannot use your rights yet.")"""
+#count = 2
+#while count < 4:
+#    print("Hello brahma how are you")
+#    count += 2
+#for i in range(6):
+ #   if i == 7:
+ #       break
+ #   print(i)
+#def data():
+ #   print("Hello, welcome!")
+
+#data()  # Calling the function
+"""def hana(name):
+    print(f"Hello, [name]!")
+
+hana("brahma")
+hana("ravi")"""
+"""def add(p, q):
+    return p + q
+
+result = add(15, 54)
+print(result)"""
+# No parameters, no return
+def say_hello():
+    print("Hi!")
+
+# With parameters, no return
+"""def greet_person(name):
+    print(f"Hello, {name}!")
+
+# With parameters and return
+def division(r, s):
+    return r / s
+
+say_hello()
+greet_person("brahma")
+result = division(6, 4)
+print(result)"""
+
+
+
+
+
+
+
+
+
+
+
+
