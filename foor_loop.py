5ea0d59a767bb5699899b1980dc6aa8cfe0843f7
beeravalli-417 <beeravalli345@gmail.com>
2025-04-29 07:48:28 +0530

    second change

--- foor_loop.py
+++ foor_loop.py
@@ -1,3 +1,62 @@
-from math import floor
+"""fruits = ["apple", "banana", "cherry" ,"kiwi"]
 
-floor()
+for fruit in fruits:
+    print(fruit)"""
+"""numbers = (1, 2, 3, 4, 5, 6, 7, 8,)
+
+for num in numbers:
+    print(num)"""
+#student = {"name": "brahma", "age": 20}
+#student = {"name": "ravi", "age": 23}
+#student = {"name": "rama", "age": 27}
+#student = {"name": "krshna", "age": 18}
+#student = {"name": "vasu", "age": 29}
+
+#for i in range(1, 6):
+#    print("*" * i)
+#for i in range(3, 8):
+#     print("*" * i)
+#for i in range (1,20):
+#     print("*" * i)
+#loop with zip
+#numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,]
+#letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',]
+
+#for number, letter in zip(numbers, letters):
+#    print(number, letter)
+#fruits = {"apple", "banana", "cherry" "kiwi","mango",}
+#for fruit in fruits:
+#    print(fruit)
+#matrix = [[1, 2], [3, 4]]
+#total = 0
+#for row in matrix:
+#    for num in row:
+#        total += num
+#print(total)
+"""numbers = [1, 2, 3]
+doubled = []
+for num in numbers:
+    doubled.append(num * 2)
+print(doubled)"""
+"""colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    if color == "yellow":
+        print("Found yellow!")
+        break
+    print(color)"""
+"""for i in range(1, 4):
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i*j}")
+    print()  # Empty line after each table"""
+"""for  i in range(1,10):
+    for j in range(1,11):
+        print(f"{i} x {j} ={i*j}")
+    print()  #empty line after each table  """
+
+
+
+
+
+
+
+
